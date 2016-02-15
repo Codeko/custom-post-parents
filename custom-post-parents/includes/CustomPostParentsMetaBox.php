0e54d9bbc0ee139e0f7460d5fdc70d58355b356b
Boris Burgos <bburgos@codeko.com>
2016-02-15 11:15:23 +0100

    refs #7865

--- custom-post-parents/includes/CustomPostParentsMetaBox.php
+++ custom-post-parents/includes/CustomPostParentsMetaBox.php
@@ -14,7 +14,7 @@ class CustomPostParentsMetaBox {
         add_action('admin_menu', function() {
             $optionSchemas = CustomPostParentsGlobal::instance()->get_post_type_schema();
             // RECORRER CUSTOM POST TYPES CON PADRES ESPECIALES Y DESHABILITAR EL PAGEPARENTDIV PARA CADA UNO DE ELLOS
-            foreach ($optionSchemas as $pageType) {
+            foreach ($optionSchemas as $pageType => $v) {
                 remove_meta_box('pageparentdiv', $pageType, 'normal');
             }
         });
@@ -28,7 +28,7 @@ class CustomPostParentsMetaBox {
         $isFirst = true;
         foreach ($optionSchemas as $ownerPageType => $schema) {
             add_meta_box(
-                    'custom-post-parents-metabox', __('Jerarquia', 'custom-post-parents'), array($this, 'add_meta_boxes_add_callback'), $ownerPageType, 'side', 'high', array(
+                    'custom-post-parents-metabox','page' == $ownerPageType ? __('Page Attributes') : __('Attributes'), array($this, 'add_meta_boxes_add_callback'), $ownerPageType, 'side', 'high', array(
                 'schema' => $schema
                     )
             );
@@ -43,18 +43,20 @@ class CustomPostParentsMetaBox {
         $post_type_object = get_post_type_object($post->post_type);
 
         $isFirst = true;
-
+        ?>
+    <label class="screen-reader-text" for="parent_id"><?php _e('Parent') ?></label>
+    <?php
         echo "<p><strong>" . __('Padre actual', 'custom-post-parents') . ":</strong>
                 <span id='parent_id_status'>
                 " . ($post->post_parent ? get_the_title($post->post_parent) : __('(no parent)') ) . "
                 </span></p>
                 <input type='hidden' id='parent_id' name='parent_id' value='{$post->post_parent}' />";
 
-        echo "<p><strong>". __('Tipo de post').": </strong> 
+        echo "<p><strong>" . __('Tipo de post') . ": </strong> 
         <select name='my_meta_box_post_type' id='my_meta_box_post_type'>";
         $post_types = get_post_types('', 'objects');
         foreach ($post_types as $post_type) {
-            if ($post_type->hierarchical == true && in_array($post_type->name, $schema["values"]) ) {
+            if ($post_type->hierarchical == true && in_array($post_type->name, $schema["values"])) {
                 echo "<option value = '";
                 echo esc_attr($post_type->name);
                 echo "' ";
@@ -73,12 +75,13 @@ class CustomPostParentsMetaBox {
         foreach ($schema['values'] as $k => $pageType) {
             $pages = wp_dropdown_pages(array(
                 'post_type' => $pageType,
+                'exclude_tree'     => $post->ID,
                 'selected' => $post->post_parent,
                 'name' => 'selection-parent-id-' . $pageType,
                 'id' => 'selection-parent-id-' . $pageType,
                 'show_option_none' => __("Ninguno"),
                 'option_none_value' => '0',
-                 'hierarchical' => 0,
+                'hierarchical' => 0,
                 'sort_column' => 'menu_order, post_title',
                 'echo' => 0)
             );
@@ -101,16 +104,16 @@ class CustomPostParentsMetaBox {
                 <?php
             }
 
-            
-                $label = $lookup[$pageType];
-                echo "<div id='" . $pageType . "-container' class='padres-containers'><p><strong>{$label}: </strong> ";
-                if (!empty($pages)) {
-                    echo $pages;
-                }else{
-                    echo __('none');
-                }
-                echo "</p></div>";
-            
+
+            $label = $lookup[$pageType];
+            echo "<div id='" . $pageType . "-container' class='padres-containers'><p><strong>{$label}: </strong> ";
+            if (!empty($pages)) {
+                echo $pages;
+            } else {
+                echo __('none');
+            }
+            echo "</p></div>";
+
             $isFirst = false;
         }
         ?><script>
@@ -124,7 +127,47 @@ class CustomPostParentsMetaBox {
                     $("#" + tipo + "-container").show();
                 });
             });
-        </script><?php
+        </script>
+        <hr/>
+        <?php
+        if ('page' == $post->post_type && 0 != count(get_page_templates($post)) && get_option('page_for_posts') != $post->ID) {
+            $template = !empty($post->page_template) ? $post->page_template : false;
+            ?>
+            <p><strong><?php _e('Template') ?></strong><?php
+                /**
+                 * Fires immediately after the heading inside the 'Template' section
+                 * of the 'Page Attributes' meta box.
+                 *
+                 * @since 4.4.0
+                 *
+                 * @param string  $template The template used for the current post.
+                 * @param WP_Post $post     The current post.
+                 */
+                do_action('page_attributes_meta_box_template', $template, $post);
+                ?></p>
+            <label class="screen-reader-text" for="page_template"><?php _e('Page Template') ?></label><select name="page_template" id="page_template">
+                <?php
+                /**
+                 * Filter the title of the default page template displayed in the drop-down.
+                 *
+                 * @since 4.1.0
+                 *
+                 * @param string $label   The display value for the default page template title.
+                 * @param string $context Where the option label is displayed. Possible values
+                 *                        include 'meta-box' or 'quick-edit'.
+                 */
+                $default_title = apply_filters('default_page_template_title', __('Default Template'), 'meta-box');
+                ?>
+                <option value="default"><?php echo esc_html($default_title); ?></option>
+                <?php page_template_dropdown($template); ?>
+            </select>
+        <?php }
+        ?>
+        <p><strong><?php _e('Order') ?></strong></p>
+        <p><label class="screen-reader-text" for="menu_order"><?php _e('Order') ?></label><input name="menu_order" type="text" size="4" id="menu_order" value="<?php echo esc_attr($post->menu_order) ?>" /></p>
+            <?php if ('page' == $post->post_type && get_current_screen()->get_help_tabs()) { ?>
+            <p><?php _e('Need help? Use the Help tab in the upper right of your screen.'); ?></p>
+            <?php
+        }
     }
-
 }
