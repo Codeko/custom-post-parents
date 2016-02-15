7dddfe97e2f10e5bfa1ba9ca27a876b9a82d69a3
Boris Burgos <bburgos@codeko.com>
2016-02-15 13:12:03 +0100

    refs #7682

--- custom-post-parents/includes/CustomPostParents.php
+++ custom-post-parents/includes/CustomPostParents.php
@@ -23,18 +23,40 @@ if (!class_exists('CustomPostParents')) {
             add_action('init', array(&$this, 'rewrite_perso_posts'));
             add_action('pre_get_posts', array(&$this, 'set_array_cpt'));
             add_filter('parse_query', array(&$this, 'set_array_cpt'));
+            add_filter('rewrite_rules_array', array(&$this, 'clear_rewrites'));
+            add_filter('register_post_type_args', array(&$this, 'update_post_types'), 10, 2);
         }
 
+        function update_post_types($args, $post_type ){
+            $optionSchemas = CustomPostParentsGlobal::instance()->get_post_type_schema();
+            if(array_key_exists($post_type, $optionSchemas)){
+                $args["rewrite"]["slug"]="/";
+            }
+            return $args;
+        }
+        
+        function clear_rewrites($rules){
+            $optionSchemas = CustomPostParentsGlobal::instance()->get_post_type_schema();
+            //Eliminamos los rewrites generados por los post types sobre los que trabajamos
+            $regexp="/".join("|", array_keys($optionSchemas))."=/";
+            foreach ($rules as $rule => $rewrite) {
+                if( preg_match($regexp,$rewrite) ) {
+                    unset($rules[$rule]);
+                }
+            }
+            return $rules;
+        }
+        
         /*
          * Permite encadenar n niveles en la url
          */
 
         function rewrite_perso_posts() {
             add_rewrite_rule(
-                    '.*/([^/]+)/?$', 'index.php?attachment=$matches[1]', 'top'
+                    '.*/([^/]+)/?$', 'index.php?attachment=$matches[1]', 'bottom'
             );
             add_rewrite_rule(
-                    '(.*)/?$', 'index.php?attachment=$matches[1]', 'top'
+                    '(.*)/?$', 'index.php?attachment=$matches[1]', 'bottom'
             );
         }
 
@@ -42,10 +64,9 @@ if (!class_exists('CustomPostParents')) {
          * Incluye todos los tipos de post para la consulta que obtiene el post solicitado (solamente en el frontend)
          */
 
-        function set_array_cpt($query) {
-            global $wp_query;
-            if (!is_admin() && $wp_query->is_main_query() && $wp_query->is_singular()) {
-                $query->set('post_type', 'any');
+        function set_array_cpt($query) {            
+            if (!is_admin() && $query->is_main_query() && $query->is_singular()) {
+                  $query->set('post_type', "any");
             }
             return $query;
         }
@@ -60,7 +81,7 @@ if (!class_exists('CustomPostParents')) {
             if (!is_home() && !is_admin()) {
                 $permalink = get_permalink();
                 $url = home_url(add_query_arg(array(), $wp->request)) . '/';
-                if (is_single() && strcmp($permalink, $url) != 0) {
+                if (is_singular() && strcmp($permalink, $url) != 0) {
                     wp_redirect($permalink, 301);
                 }
             }
