53ea5a733e8a4f27899b080ba62e8ce06273b704
Boris Burgos <bburgos@codeko.com>
2017-03-20 11:57:43 +0100

    refs #8824 Flush rewrites on deactivation

--- custom-post-parents/custom-post-parents.php
+++ custom-post-parents/custom-post-parents.php
@@ -5,9 +5,14 @@
   License: GPLv2
  */
 
+function custom_post_parents_deactivation(){
+    //Clear custom rewrites
+    flush_rewrite_rules();
+}
+register_deactivation_hook( __FILE__, 'custom_post_parents_deactivation' );
+
 define('CUSTOM_POST_PARENTS_PATH', dirname(__FILE__));
 define('CUSTOM_POST_PARENTS_FILE', __FILE__);
-
 
 foreach ( glob( plugin_dir_path( __FILE__ ) . "includes/*.php" ) as $file ) {
     include_once $file;
