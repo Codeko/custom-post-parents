55648afa615928f50b80167a760ecaf6c30e4419
oscar <oscar@oscar-H81M-S2H>
2017-03-28 12:03:35 +0200

    Mejorada la asignación de padres en el punto de configuración

--- js/custom-post-parents-admin.js
+++ js/custom-post-parents-admin.js
@@ -3,12 +3,62 @@
         var $ul = $('.custom-post-parents-selectedlist');
         var $adders = $('.custom-post-parents-selectlist-add');
 
-        $ul.on('click', '.ntdelbutton', function () {
-            $(this).parents('span').remove();
-        });
-
         var cache = {};
-        $adders.autocomplete({
+                        $.post(ajaxurl, {
+                        'action': 'custom-post-parents_search_post_types'
+                    },
+                    function (result) {
+                            var it=$("<option>").attr("name","custom-post-parents-post_types");
+                            it.text("Choose option:");
+                            $('.custom-post-parents-selectlist-add').append(it);
+                        $.each(result.data.lookup, function (index, item) {
+                            cache[index] = {value: index, label: item};
+                            var it=$("<option>").attr("name","custom-post-parents-post_types").attr("id",item);
+                            it.text(item);
+                            $('.custom-post-parents-selectlist-add').append(it);
+                        });
+                    }
+                );
+       /* var tags=[{"nombre":"","padre":""},{"nombre":"persona","padre":"page"},{"nombre":"pepe","padre":"proyecto"},{"nombre":"juan","padre":"page"},{"nombre":"manolo","padre":"persona"}];
+        $.each(tags,function(i,v)
+        {
+            var item=$("<option>").attr("name","custom-post-parents-post_types");
+            item.text(v.nombre);
+            $('.custom-post-parents-selectlist-add').append(item);   
+        })*/
+        //$('.custom-post-parents-selectlist-add').change(function(event){
+        $('.custom-post-parents-selectlist-add').change(function(event){
+            var s=event.target;
+            var section=s.getAttribute("data-section")
+            var selectedElements = $('input[name="custom-post-parents-post_types['+section+'][]"]');
+            var currentVals = [];
+            var valActual=$(this).val();
+            var seleccionado=$("#cpt-f-"+section+" option:selected");
+            if(valActual!="Choose option:")
+                $("#cpt-f-"+section+" option:selected").hide();
+            $.each(selectedElements, function (i,v) {
+                    currentVals.push($(this).val());
+                });
+               var v=$.inArray(valActual, currentVals) === -1;
+            if ($.inArray(valActual, currentVals) === -1 && valActual!="Choose option   :") {
+                    var s=event.target;
+                    var section=s.getAttribute("data-section");
+                    var ite = $([
+                         '<span>',
+                         '<input name="custom-post-parents-post_types['+section+'][]" type="hidden" value="'+valActual+'"/>',
+                         "<a name='"+valActual+"'class='ntdelbutton'><span class='remove-tag-icon' aria-hidden='true'></span></a>&nbsp;",
+                         valActual,
+                         '</span>'
+                     ].join(''));
+                             $ul.on('click', '.ntdelbutton',mostrar(section));
+                     $("div[data-section='"+section+"']").append(ite);
+            }
+                
+                //$(".custom-post-parents-selectlist-add option:selected").remove();
+        })
+        
+        
+       /* $adders.autocomplete({
             minLength: 0,
             source: function( request, response ) {
                 var term = request.term;
@@ -48,14 +98,32 @@
                     ].join(''));
                     $('.custom-post-parents-selectedlist[data-section="'+section+'"]').append(item);
                 }
+                /*if ($.inArray(type, currentVals) === -1) {
+                    var item = $([
+                        '<option name="custom-post-parents-post_types['+section+'][]" type="hidden" value="'+type+'">',
+                        label,
+                        '</option>'].join(''));
+                    $('.custom-post-parents-selectedlist[data-section="'+section+'"]').append(item);
+                }
 
                 $input.val('');
                 return false;
             }
         }).focus(function(){       
             $(this).autocomplete( "search",$(this).val());
-        });;
+            //$(this)
+        })*/;;
 
 
     });
+    function mostrar(section)
+    {
+        return function(e)
+        {
+            e.preventDefault();
+            var texto=$(this).attr("name");
+            $("#cpt-f-"+section+" >option[id='"+texto+"']").show();
+            $(this).parents('span').remove();
+        }
+    }
 })(jQuery);
